b1387a30664a43801e3560f32cbb6d010ec6053e
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2024-08-08 23:49:36 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ app.use("/dashboard",dashboardRoutes)
 app.use("/products",productRoutes)
 app.use("/users", userRoutes); // http://localhost:8000/users
 app.use("/expenses", expenseRoutes); // http://localhost:8000/expenses
-
+//update dey
 /* server */
 const port = Number(process.env.PORT) || 3001;
 app.listen(port,"0.0.0.0" ,() => {
